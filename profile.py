1f5a7ce1ebcf4fc821369d259924ca24677169ba
shivam675 <43983114+shivam675@users.noreply.github.com>
2018-11-27 12:52:24 +0530

    Add files via upload

---
+++ profile.py
@@ -0,0 +1,7 @@
+#qpy:3
+#qpy:console
+a=input("Enter Name")
+b=input("Enter Roll No.")
+c=input("Enter Gr No.")
+d=input("Enter Branch")
+print("Name of the student is:"+a,"His/Her Roll No. is:"+b,"And Gr No.:"+c,"And Branch being:"+d)
