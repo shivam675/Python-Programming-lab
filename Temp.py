a4f6565526e15ffad82b173cac78e88dd525a836
shivam675 <43983114+shivam675@users.noreply.github.com>
2018-11-27 13:10:52 +0530

    Add files via upload

---
+++ Temp.py
@@ -0,0 +1,9 @@
+j=int(input("enter temp"))
+k=int(input("if temp enteted is in celcius enter 1 or else press 2"))
+if k==1:
+ a=(9*j +160)/5
+ print("temp in farenheit is",a)
+else:
+ a=(5*j -160)/9
+ print("temp in celcius is:",a)
+
