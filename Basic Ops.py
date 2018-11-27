2ad8f97d4eef5e5e0507b930e61045c2136bf470
shivam675 <43983114+shivam675@users.noreply.github.com>
2018-11-27 13:19:21 +0530

    Add files via upload

---
+++ Basic Ops.py
@@ -0,0 +1,12 @@
+x=int(input("enter first no."))
+y=int(input("dnter second no."))
+print("1-add,2-substract,3-multiply,4-divide")
+choice=int(input("enter choice"))
+if choice==1:
+ print("choice is addtn",x+y)
+elif choice==2:
+ print("choice is substraction",x-y)
+elif choice==3:
+ print("choice is multiplication",x*y)
+elif choice==4:
+print("choice is division",x%y)
